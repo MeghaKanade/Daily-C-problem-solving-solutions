0578e68c01a09e6f87f0264c3dc8d8005442bd17
Megha Kanade <meghakanade5422@gmail.com>
2022-01-05 16:27:46 +0530

    My 1st commit

---
+++ Factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num,i,fact=1;
+    printf("Enter the number");
+    scanf("%d",&num);
+    for(i=2;i<=num;i++)
+    {
+        fact=fact*i;
+        
+    }
+    printf("Factorial of %d is %d", num,fact);
+    return 0;
+
+}
