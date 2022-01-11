e1572fbd374e830780e9d41fa6690361e6479a52
Megha Kanade <meghakanade5422@gmail.com>
2022-01-11 22:57:34 +0530

    ASCII for character

---
+++ ascii.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch;
+    printf("Enter the character");
+    scanf("%c",&ch);
+    printf("ASCII value of %c is %d",ch,ch);
+    return 0;
+}
