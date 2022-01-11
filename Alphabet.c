a9b7a82664fb7d6c75e59a454875812266b2770b
Megha Kanade <meghakanade5422@gmail.com>
2022-01-11 22:29:30 +0530

    Checking whether a Character is Alphabet or Not

---
+++ Alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char a;
+    printf("Enter the character");
+    scanf("%c",&a);
+    if(ch>='a' && ch<='z') || (ch>='A' && ch<='Z')
+    {
+        printf("Inserted character %c is an alphabet",a);
+    }
+    else()
+    {
+        printf("Inserted character %c is not alphabet",a);
+    }
+    return 0;
+    
+}
